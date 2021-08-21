394f70a0bdcdba7b46b13728e59e30c52e0f24cc
Alexandre Artaud <alexandre.artaud@etu.univ-grenoble-alpes.fr>
2021-08-21 20:39:07 +0200

    Export Client_TOKEN

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const RoombaClient = require("./structures/RoombaClient");
+const { CLIENT_TOKEN } = require('./util/config');
 
 let client = new RoombaClient({
     prefix: '!'
 });
 
-client.login("");
+client.login(CLIENT_TOKEN);
