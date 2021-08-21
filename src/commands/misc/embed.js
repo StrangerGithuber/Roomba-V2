f32398a8d26163fd781dff773b25dc62b23ee5a3
Alexandre Artaud <alexandre.artaud@etu.univ-grenoble-alpes.fr>
2021-08-21 20:36:35 +0200

    First commit

---
+++ src/commands/misc/embed.js
@@ -0,0 +1,14 @@
+const { Command } = require('discord-akairo');
+
+class EmbedCommand extends Command {
+    constructor() {
+        super('embed', {
+            aliases: ['embed']
+        });
+    }
+
+    exec(message) {
+
+    }
+}
+module.exports = EmbedCommand;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ let client = new RoombaClient({
     prefix: '!'
 });
 
-client.login("ODc3OTM4OTAxMDU3MTQ2OTEw.YR56UQ.7arhARk2E1u2-p5ij5n-Lx0Z-gQ");
+client.login("");
