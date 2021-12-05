be372dd74c740e19c3c2afe20ee3b55143fe9bfd
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2021-12-05 22:07:19 +0100

    Added generation of log file through command "log"

--- src/commands/moderation/log.js
+++ src/commands/moderation/log.js
@@ -35,7 +35,7 @@ class LogCommand extends Command {
         const basicLogs = message.author.id === this.client.ownerID ? await this.client.log.base.getLogs() : await this.client.log.base.getLogs(message.guild.id);
         const musicLogs = message.author.id === this.client.ownerID ? await this.client.log.music.getLogs() : await this.client.log.music.getLogs(message.guild.id);
         const defaultTypes = ["global", "command", "log", "info", "warning", "error"];
-        const defaultLoggers = ["base", "music"];
+        const defaultLoggers = ["base", "music", "all"];
         if (loggerType) loggerType = loggerType.toLowerCase();
         if (loggerType !== null && !defaultLoggers.includes(loggerType)) return message.channel.send("Le type de logger que vous souhaitez récupérer n'existe pas");
         if (typeFilter) typeFilter = typeFilter.toLowerCase();
@@ -94,6 +94,98 @@ class LogCommand extends Command {
                     })
                     break;
                 case "music":
+                    fs.writeSync(fd, `\n\n\n███████████████████████████████████████████████████████████████████████████████████████████████████████████████████\n`);
+                    fs.writeSync(fd, `████████████████████████████████████████████████████ MUSIC LOGGER █████████████████████████████████████████████████\n`);
+                    fs.writeSync(fd, `███████████████████████████████████████████████████████████████████████████████████████████████████████████████████\n\n`);
+                    types.forEach(type => {
+                        if (musicLogs[type]) {
+                            switch (type){
+                                case "global":
+                                    fs.writeSync(fd, `\n`);
+                                    musicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "command":
+                                    fs.writeSync(fd, `\n`);
+                                    musicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.user.userTag} (${log.user.userID}) : command ${log.command.name} ${log.command.data === null ? "" : "with data -> " + JSON.stringify(log.command.data)} \n`);
+                                    });
+                                    break;
+                                case "log":
+                                    fs.writeSync(fd, `\n`);
+                                    musicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "info":
+                                    fs.writeSync(fd, `\n`);
+                                    musicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] Event : "${log.event.name}" ${log.event.data === null ? "" : "with data -> " + JSON.stringify(log.event.data)} \n`);
+                                    });
+                                    break;
+                                case "warning":
+                                    fs.writeSync(fd, `\n`);
+                                    musicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "error":
+                                    fs.writeSync(fd, `\n`);
+                                    musicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}]${log.error}\n`);
+                                    });
+                                    break;
+                            }
+                        }
+                    })
+                    break;
+                case "all":
+                    fs.writeSync(fd, `███████████████████████████████████████████████████████████████████████████████████████████████████████████████████\n`);
+                    fs.writeSync(fd, `████████████████████████████████████████████████████ BASIC LOGGER █████████████████████████████████████████████████\n`);
+                    fs.writeSync(fd, `███████████████████████████████████████████████████████████████████████████████████████████████████████████████████\n\n`);
+                    types.forEach(type => {
+                        if (basicLogs[type]) {
+                            switch (type){
+                                case "global":
+                                    fs.writeSync(fd, `\n`);
+                                    basicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "command":
+                                    fs.writeSync(fd, `\n`);
+                                    basicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.user.userTag} (${log.user.userID}) : command ${log.command.name} ${log.command.data === null ? "" : "with data -> " + JSON.stringify(log.command.data)} \n`);
+                                    });
+                                    break;
+                                case "log":
+                                    fs.writeSync(fd, `\n`);
+                                    basicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "info":
+                                    fs.writeSync(fd, `\n`);
+                                    basicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "warning":
+                                    fs.writeSync(fd, `\n`);
+                                    basicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}] ${log.message}\n`);
+                                    });
+                                    break;
+                                case "error":
+                                    fs.writeSync(fd, `\n`);
+                                    basicLogs[type].forEach(log => {
+                                        fs.writeSync(fd, `[${type.toUpperCase()}][Base Logger][${convertToEuropeanDate(log.date)}][${log.guildID}]ERROR : ${log.message}\n`);
+                                    });
+                                    break;
+                            }
+                        }
+                    })
                     fs.writeSync(fd, `\n\n\n███████████████████████████████████████████████████████████████████████████████████████████████████████████████████\n`);
                     fs.writeSync(fd, `████████████████████████████████████████████████████ MUSIC LOGGER █████████████████████████████████████████████████\n`);
                     fs.writeSync(fd, `███████████████████████████████████████████████████████████████████████████████████████████████████████████████████\n\n`);
